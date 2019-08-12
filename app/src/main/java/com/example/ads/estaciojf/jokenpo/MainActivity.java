7d6922cd23eb4cb98aef3b7229767219528ac274
lesimoes <lesimoes0@gmail.com>
2019-08-12 20:39:27 -0300

    1- Primeiro commit

---
+++ app/src/main/java/com/example/ads/estaciojf/jokenpo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ads.estaciojf.jokenpo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
