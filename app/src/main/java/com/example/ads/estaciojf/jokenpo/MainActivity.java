f1a1244bb6bd09ec8ca97e214211a6885f0e9dea
lesimoes <lesimoes0@gmail.com>
2019-08-15 18:07:23 -0300

    aula

--- app/src/main/java/com/example/ads/estaciojf/jokenpo/MainActivity.java
+++ app/src/main/java/com/example/ads/estaciojf/jokenpo/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,8 +14,12 @@ public class MainActivity extends AppCompatActivity {
     Button btnPapel;
     Button btnTesoura;
     Button btnPedra;
-    Button btnTeste;
-    ImageView img;
+    ImageView imgPlayer1;
+    ImageView imgPlayer2;
+    TextView textPlacar;
+
+    int pontuacaoPlayer1;
+    int pontuacaoPlayer2;
 
     String btn;
     Context ctx;
@@ -25,19 +30,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ctx = this;
 
+        pontuacaoPlayer1 = 0;
+        pontuacaoPlayer2 = 0;
+
         btnPapel = findViewById(R.id.btnPapel);
         btnTesoura = findViewById(R.id.btnTesoura);
         btnPedra = findViewById(R.id.btnPedra);
-        btnTeste = findViewById(R.id.button4);
-        img = findViewById(R.id.imageView);
+        imgPlayer1 = findViewById(R.id.imgPlayer1);
+        imgPlayer2 = findViewById(R.id.imgPlayer2);
+        textPlacar = findViewById(R.id.textPlacar);
+
+        imgPlayer1.setVisibility(View.GONE);
 
 
         btnPapel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Papel", Toast.LENGTH_LONG).show();
+
+
+                Toast.makeText(getApplicationContext(), "Papel", Toast.LENGTH_LONG)
+                        .show();
+
+                Toast msg = Toast.makeText(getApplicationContext(), "Papel embrulha pedra!Você Venceu!!!", Toast.LENGTH_SHORT);
+                msg.show();
+
                 btn = "Papel";
-                img.setImageResource(R.drawable.tesoura);
+                imgPlayer1.setImageResource(R.drawable.papel);
+                imgPlayer1.setVisibility(View.VISIBLE);
+                pontuacaoPlayer1 ++;
+
+                textPlacar.setText(pontuacaoPlayer1 + " x " + pontuacaoPlayer2);
 
             }
         });
@@ -47,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast.makeText(ctx, "Tesoura", Toast.LENGTH_LONG).show();
                 btn = "Tesoura";
+                imgPlayer1.setImageResource(R.drawable.tesoura);
             }
         });
 
@@ -55,18 +78,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast.makeText(ctx, "Pedra", Toast.LENGTH_LONG).show();
                 btn = "Pedra";
+                imgPlayer1.setImageResource(R.drawable.pedra);
             }
         });
 
 
-        btnTeste.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(ctx, btn, Toast.LENGTH_LONG).show();
 
-
-            }
-        });
 
 
 
